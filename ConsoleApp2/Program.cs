47be4f0984e5ca4e7d83ce1a8e099948dc6ce003
h0ly5h1t <124813325+h0ly5h1t@users.noreply.github.com>
2023-03-07 17:14:48 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool passTrue = true;
+            Console.WriteLine("Введите длину пароля:");
+            int.TryParse(Console.ReadLine(), out int symCount);
+            while (passTrue)
+            {
+                if (symCount < 8)
+                {
+                    Console.WriteLine("Минимальная длина пароля составляет - 8 символов!");
+                    Console.ReadKey();
+                    passTrue = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Ваш пароль - \u001b[32;1m{PassGenerator.RandomPass(symCount)}\u001b[0m\nДлина пароля - {symCount}");
+                    Console.WriteLine("Закройте программу или нажмите любую клавишу, чтобы сгенерировать новый пароль!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
+        }
+
+        class PassGenerator
+        {
+            public static string RandomPass(int symCount)
+            {
+                Random randomChar = new Random();
+                StringBuilder charList = new StringBuilder();
+
+                for (int i = 0; i < symCount; i++)
+                    charList.Append((char)randomChar.Next(33, 127));
+
+                return charList.ToString();
+            }
+        }
+    }
+}
